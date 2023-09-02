9072faea0ba7752564fb9722f9eea8921cb4ea3c
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-02 10:36:57 -0300

    feat: criação dos v-text-fields, select e button da pagina  e aplicação dos v-models

--- project-trainsyst/src/views/UserRegistration/UserRegistration.vue
+++ project-trainsyst/src/views/UserRegistration/UserRegistration.vue
@@ -1,3 +1,62 @@
 <template>
-    <h1>Cadastro de Usuario</h1>
+    <h1>Crie sua conta</h1>
+    <v-form ref="form" @submit.prevent="handleSubmit">
+        <v-text-field 
+        label="Nome"
+        variant="outlined"
+        v-model="name"
+        >
+        </v-text-field> 
+        <v-text-field 
+        label="E-mail"
+        variant="outlined"
+        v-model="email"
+        >
+        </v-text-field> 
+        <v-text-field 
+        label="Senha"
+        variant="outlined"
+        v-model="password"
+        >
+        </v-text-field> 
+        <v-text-field 
+        label="Confirme sua senha"
+        variant="outlined"
+        >
+        </v-text-field> 
+
+        <v-select
+        label="Selecione o seu plano"
+        :items="['Bronze', 'Prata', 'Ouro']"
+        v-model="type_plan"
+        >
+        
+        </v-select>
+        
+        <v-btn type="submit">Cadastrar</v-btn>
+        <v-btn>Voltar</v-btn>
+
+    </v-form>
+
 </template>
+
+<script>
+export default{
+    data() {
+        return {
+            name: "",
+            email: "",
+            password: "",
+            type_plan: []
+
+        }
+    },
+
+    methods: {
+        handleSubmit() {
+
+        }
+    }
+}
+
+</script>
