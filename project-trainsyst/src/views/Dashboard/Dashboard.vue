df5d7942d40768b3abf258d72ab5c9f1911096af
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-04 21:40:50 -0300

    feat: cria base do template e script da pagina no projeto

--- project-trainsyst/src/views/Dashboard/Dashboard.vue
+++ project-trainsyst/src/views/Dashboard/Dashboard.vue
@@ -1,5 +1,33 @@
 <template>
+  <h1>Bem vindo, {{ usuarioLogado }}</h1>
+  <v-container>
+    <v-card variant="outlined">
+      <v-card-title>Alunos Cadastrados</v-card-title>
+      <v-card-text>{{ alunosCadastrados }}</v-card-text>
+      <v-card-actions>
+        <v-btn>Adicionar</v-btn>
+      </v-card-actions>
+    </v-card>
 
-<h1>Dashboard</h1>        
-    
+    <v-card variant="outlined">
+      <v-card-title>Exercícios</v-card-title>
+      <v-card-text>{{ exercicios }}</v-card-text>
+      <v-card-actions>
+        <v-btn>Adicionar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      alunosCadastrados: 0,
+      exercicios: 0,
+      usuarioLogado: "",
+    };
+  },
+};
+</script>
