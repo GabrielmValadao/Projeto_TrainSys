f1502e8ef9cc023081c8ba341b84e7732fc7a501
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-05 13:28:38 -0300

    feat: desenvolve a base do template e a variavel newExercise

--- project-trainsyst/src/views/ExerciseManagement/ExerciseManagement.vue
+++ project-trainsyst/src/views/ExerciseManagement/ExerciseManagement.vue
@@ -1,5 +1,27 @@
 <template>
+  <h1>Exercícios</h1>
+  <v-text-field
+    v-model="newExercise"
+    label="Nome do exercício"
+    variant="underlined"
+    required 
+  />
+  <v-btn>Cadastrar exercício</v-btn>
 
-<h1>Gerenciamento de exercicios</h1>        
-    
-</template>
+  <v-list>
+    <v-list-item>
+        
+    </v-list-item>
+
+  </v-list>
+ 
+
+<script>
+export default {
+  data() {
+    return {
+        newExercise: [],
+    };
+  },
+};
+</script>
