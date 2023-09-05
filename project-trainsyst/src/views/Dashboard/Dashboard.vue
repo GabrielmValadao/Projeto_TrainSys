5d46e115b134f6af97dd2e2eca1ca538df36c3dd
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-04 22:20:54 -0300

    style: ajustes de posição das variaveis no v-card

--- project-trainsyst/src/views/Dashboard/Dashboard.vue
+++ project-trainsyst/src/views/Dashboard/Dashboard.vue
@@ -2,16 +2,20 @@
   <h1>Bem vindo(a), {{ usuarioLogado }}</h1>
   <v-form @submit.prevent="handleSubmit">
     <v-card variant="outlined">
-      <v-card-title>Alunos Cadastrados</v-card-title>
-      <v-card-text>{{ alunosCadastrados }}</v-card-text>
+      <v-card-title
+        >Alunos Cadastrados
+        {{ alunosCadastrados }}
+      </v-card-title>
       <v-card-actions>
         <v-btn type="submit">Adicionar</v-btn>
       </v-card-actions>
     </v-card>
 
     <v-card variant="outlined">
-      <v-card-title>Exercícios</v-card-title>
-      <v-card-text>{{ exercicios }}</v-card-text>
+      <v-card-title>
+        Exercícios
+        {{ exercicios }}
+      </v-card-title>
       <v-card-actions>
         <v-btn type="submit">Adicionar</v-btn>
       </v-card-actions>
@@ -20,39 +24,39 @@
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
   data() {
     return {
       alunosCadastrados: 0,
       exercicios: 0,
-      usuarioLogado: localStorage.getItem('user_name'),
+      usuarioLogado: localStorage.getItem("user_name"),
     };
   },
 
   mounted() {
-    this.fetchData()
+    this.fetchData();
   },
 
   methods: {
     fetchData() {
-        axios({
-          url: "http://localhost:3000/dashboard",
-          method: "GET",
-          data: {
-            alunosCadastrados: this.this.alunosCadastrados,
-            exercicios: this.exercicios,
-          },
-        })
+      axios({
+        url: "http://localhost:3000/dashboard",
+        method: "GET",
+        data: {
+          alunosCadastrados: this.this.alunosCadastrados,
+          exercicios: this.exercicios,
+        },
+      })
         .then((response) => {
-            this.alunosCadastrados = response.data.alunosCadastrados;
-            this.exercicios = response.data.exercicios
+          this.alunosCadastrados = response.data.alunosCadastrados;
+          this.exercicios = response.data.exercicios;
         })
         .catch((error) => {
-            console.log(error)
-            alert('Erro ao buscar os dados:', error)
-        })
-    }
-  }
+          console.log(error);
+          alert("Erro ao buscar os dados:", error);
+        });
+    },
+  },
 };
 </script>
