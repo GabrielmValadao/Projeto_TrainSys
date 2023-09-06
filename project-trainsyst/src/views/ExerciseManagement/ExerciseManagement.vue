6427c9deca03878e47d161a58b8d37e4d8ed7bb0
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-05 22:53:29 -0300

    feat: desenvolve o mounted e methods do projeto, utilizando o axios

--- project-trainsyst/src/views/ExerciseManagement/ExerciseManagement.vue
+++ project-trainsyst/src/views/ExerciseManagement/ExerciseManagement.vue
@@ -1,27 +1,77 @@
 <template>
   <h1>Exercícios</h1>
-  <v-text-field
-    v-model="newExercise"
-    label="Nome do exercício"
-    variant="underlined"
-    required 
-  />
-  <v-btn>Cadastrar exercício</v-btn>
+  <v-form @submit.prevent="addExercises">
+    <v-text-field
+      v-model="newExercise"
+      label="Nome do exercício"
+      variant="underlined"
+      required
+    />
+    <v-btn>Cadastrar exercicio</v-btn>
 
-  <v-list>
-    <v-list-item>
-        
-    </v-list-item>
-
-  </v-list>
- 
+    <v-list>
+      <v-list-item v-for="exercise in Exercise" :key="exercise.id"> 
+      <v-list-item-title>
+        {{ exercise.data }}
+      </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-form>
+</template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-        newExercise: [],
+      newExercise: "",
+      exercises: []
     };
+  },
+
+  mounted : {
+      Exercise() {
+      return this.exercise((exercise) => 
+      exercise.includes(this.exercise.data))
+    }
+  },
+
+  methods: {
+     fetchExercises() {
+        axios({
+          url: "http://localhost:3000/exercises",
+          method: "GET",
+          data: {
+            exercises: []
+          },
+        })
+    },
+
+    addExercises() {
+      try {
+
+        axios({
+          url: "http://localhost:3000/exercises",
+          method: "POST",
+          data: {
+            newExercise: ''
+          },
+        })
+          .then((response) => {
+            console.log(response)
+          alert('Exercicio cadastrado com sucesso')
+          this.fetchExercises()
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Erro ao buscar o exercício')
+        })
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
 };
 </script>
