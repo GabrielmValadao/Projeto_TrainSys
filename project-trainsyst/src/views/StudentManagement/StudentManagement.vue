8b61f4c747dbc1f53dd4b172a576f5eb9c3fce9a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-07 18:19:28 -0300

    feat: altera o titulo da pagina e desenvolve o botão de cadastro de novo aluno

--- project-trainsyst/src/views/StudentManagement/StudentManagement.vue
+++ project-trainsyst/src/views/StudentManagement/StudentManagement.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Gerenciamento de Alunos</h1>        
+    <div>
+
+        <h1>Alunos</h1>
+        <router-link to="/cadastro/novo/aluno">
+            <v-btn>Cadastrar novo aluno</v-btn>
+        </router-link>        
+    </div>
+    
+
     </template>
