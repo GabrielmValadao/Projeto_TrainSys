93935ea3aa697e525a0d98d13a456ec2f5515fa3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-07 19:59:40 -0300

    feat: inclui os botões de visualizar e montar treino

--- project-trainsyst/src/views/StudentManagement/StudentManagement.vue
+++ project-trainsyst/src/views/StudentManagement/StudentManagement.vue
@@ -7,25 +7,70 @@
   </div>
 
   <div>
-    <v-text-field v-model="search" label="Digite o nome do aluno" ></v-text-field>
+    <v-text-field
+      v-model="search"
+      label="Digite o nome do aluno"
+      @input="filterStudents"
+    ></v-text-field>
     <v-btn>Buscar aluno</v-btn>
-  
-    <v-data-table :headers="headers" :items="filterStudents"></v-data-table>
 
+    <v-data-table :headers="headers" :items="filteredStudents">
+      <router-link to="/Cadastro/Treino">
+        <v-btn>Montar treino</v-btn>
+      </router-link>
+      <router-link to="/Visualização/Treinos">
+        <v-btn>Ver treino</v-btn>
+      </router-link>
+    </v-data-table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    data() {
-        return {
-            search: '',
-            headers: [
-                {title: 'Nome', value: 'name'},
-                {title: 'Ações', value: 'actions'}
-            ]
-        }
-        
-    }
-}
+  data() {
+    return {
+      students: [],
+      search: "",
+      headers: [
+        { title: "Nome", value: "name" },
+        { title: "Ações", value: "actions" },
+      ],
+    };
+  },
+
+  mounted() {
+    this.getStudents();
+  },
+
+  methods: {
+    getStudents() {
+      axios({
+        url: "http://localhost:3000/students",
+        method: "GET",
+      })
+        .then((response) => {
+          this.students = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Erro ao buscar aluno");
+        });
+    },
+
+    filterStudents() {
+      const searchStudent = this.search;
+      this.filteredStudents = this.students.filter((student) => {
+        return student.state.includes(searchStudent);
+      });
+    },
+  },
+
+  computed: {
+    filteredStudents() {
+      return this.students;
+    },
+  },
+};
 </script>
