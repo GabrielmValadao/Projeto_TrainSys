240677636dda04e748edf565570191a946ac7788
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-07 18:43:33 -0300

    feat: desenvolve o input de busca de aluno e inicio do script

--- project-trainsyst/src/views/StudentManagement/StudentManagement.vue
+++ project-trainsyst/src/views/StudentManagement/StudentManagement.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
+  <div>
+    <h1>Alunos</h1>
+    <router-link to="/cadastro/novo/aluno">
+      <v-btn>Cadastrar novo aluno</v-btn>
+    </router-link>
+  </div>
 
-        <h1>Alunos</h1>
-        <router-link to="/cadastro/novo/aluno">
-            <v-btn>Cadastrar novo aluno</v-btn>
-        </router-link>        
-    </div>
-    
+  <div>
+    <v-text-field v-model="search" label="Digite o nome do aluno" ></v-text-field>
+    <v-btn>Buscar aluno</v-btn>
+  </div>
+</template>
 
-    </template>
+<script>
+export default {
+    data() {
+        return {
+            search: ''
+        }
+        
+    }
+}
+</script>
