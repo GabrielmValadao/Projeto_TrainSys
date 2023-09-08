00ec62597847723f77acb7e050ad2c7ebf0afa33
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-08 12:27:55 -0300

    feat: desenvolvimento dos inputs de nome, email e contato

--- project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
+++ project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
@@ -1,6 +1,8 @@
 <template>
-
-<h1>Cadastro de Novo Aluno</h1>
-        
-    
+  <h1>Cadastro de novos alunos</h1>
+  <v-form>
+    <v-text-field type="text" label="Nome completo" />
+    <v-text-field type="email" label="Email" />
+    <v-text-field type="number" label="Contato" />
+  </v-form>
 </template>
