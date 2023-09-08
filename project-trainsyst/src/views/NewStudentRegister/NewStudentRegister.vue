e663bc5407fab0d5ecbc5935b82358889381283a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-08 16:57:32 -0300

    feat: inclui inputs de contato, data de nascimento, cep, logradouro, numero, bairro, cidade, estado e complemento

--- project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
+++ project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
@@ -1,8 +1,16 @@
 <template>
   <h1>Cadastro de novos alunos</h1>
   <v-form>
-    <v-text-field type="text" label="Nome completo" />
+    <v-text-field type="text" label="Nome completo" required/>
     <v-text-field type="email" label="Email" />
-    <v-text-field type="number" label="Contato" />
+    <v-text-field type="text" label="Contato" required/>
+    <v-text-field type="date" label="Data de nascimento" required/>
+    <v-text-field type="text" label="CEP" required/>
+    <v-text-field type="text" label="Logradouro" required/>
+    <v-text-field type="number" label="Número" required/>
+    <v-text-field type="text" label="Bairro" required/>
+    <v-text-field type="text" label="Cidade" required/>
+    <v-text-field type="text" label="Estado" required/>
+    <v-text-field type="text" label="Complemento" required/>
   </v-form>
 </template>
