b0b5182f26d9c670732e7a3c77919572d81b46df
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-11 12:29:05 -0300

    feat: desenvolvi template da pagina

--- project-trainsyst/src/views/TrainingRegistration/TrainingRegistration.vue
+++ project-trainsyst/src/views/TrainingRegistration/TrainingRegistration.vue
@@ -1,5 +1,45 @@
 <template>
-    
         <h1>Cadastro de Treino</h1>
-
+        <v-container>
+    <v-form @submit.prevent="cadastrarTreino">
+      <v-select
+        label="Exercício"
+        v-model="selectedExercise"
+        :items="exercises"
+        item-text="name"
+        item-value="id"
+        required
+      ></v-select>
+      <v-text-field
+        label="Quantidade de Repetições"
+        v-model="repetitions"
+        type="number"
+        min="1"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Peso (em kg)"
+        v-model="weight"
+        type="number"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Tempo de Pausa"
+        v-model="breakTime"
+        type="time"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Observações do Treino"
+        v-model="observations"
+      ></v-text-field>
+      <v-select
+        label="Dia da Semana"
+        v-model="selectedDay"
+        :items="daysOfWeek"
+        required
+      ></v-select>
+      <v-btn type="submit" color="primary">Cadastrar Treino</v-btn>
+    </v-form>
+  </v-container>
 </template>
