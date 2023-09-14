4daf72075ce3a305437f9464d69d496eded7f4ed
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-14 20:52:07 -0300

    feat: desenvolve o header das paginas de login e cadastro de usuario

---
+++ project-trainsyst/src/assets/components/headerLoginCadastro/headerLoginCadastro.vue
@@ -0,0 +1,11 @@
+<template>
+        <v-app-bar app color="primary">
+            <v-img src="../../../public/logo trainsys.png" max-weight="50" alt="logo TrainSys" />
+        </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'HeaderLoginCadastro'
+}
+</script>
