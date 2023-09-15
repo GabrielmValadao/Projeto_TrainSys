ce6ba4cd0eab9a28461453ec9ac3f3788a4b465c
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-14 21:52:53 -0300

    reat: cria o headerPosLogin

---
+++ project-trainsyst/src/assets/components/HeaderPosLogin/HeaderPosLogin.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app app color="primary">
+    <v-img
+      src="../../../../src/public/logo trainsys.png"
+      max-weight="50"
+      alt="logo TrainSys"
+    />
+    <router-link to="/"><h3>Home</h3> </router-link>
+    <router-link to="/Dashboard"><h3>Alunos</h3> </router-link>
+    <router-link to="/Gerenciamento/Exercicios"><h3>Exercícios</h3> </router-link>
+    <router-link to="/Gerenciamento/Aluno"><h3>Sair</h3></router-link>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "HeaderPosLogin",
+};
+</script>
