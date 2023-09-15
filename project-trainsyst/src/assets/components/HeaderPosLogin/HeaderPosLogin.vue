f30e508c49e90cad44b2e6dd26329b6838896943
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-15 19:30:08 -0300

    style: finaliza estilização do header pós login

--- project-trainsyst/src/assets/components/HeaderPosLogin/HeaderPosLogin.vue
+++ project-trainsyst/src/assets/components/HeaderPosLogin/HeaderPosLogin.vue
@@ -1,16 +1,45 @@
 <template>
-  <v-app-bar height="150" app color="rgb(67, 21, 29)">
-    <v-img
-      src="../../../../src/public/logo trainsys.png"
-      alt="logo TrainSys"
-    />
-    <v-toolbar-title>
-      <router-link to="/Dashboard">Home</router-link>
-      <router-link to="/Gerenciamento/Aluno">Alunos</router-link>
-      <router-link to="/Gerenciamento/Exercicios">Exercícios</router-link>
-      <router-link to="/">Sair</router-link>
-    </v-toolbar-title>
-  </v-app-bar>
+  <v-container>
+    <v-app-bar height="150" app color="rgb(67, 21, 29)">
+      <v-img
+        src="../../../../src/public/logo trainsys.png"
+        alt="logo TrainSys"
+        max-height="150"
+        width="100"
+        fill-height
+        tile
+      />
+
+      <div class="d-flex aling-end justify-between">
+        <v-toolbar-title>
+          <router-link to="/Dashboard"
+            >
+            <v-btn variant="outlined" color="rgb(223, 70, 97)"
+              ><v-icon icon="mdi-home" size="large"></v-icon>Home</v-btn
+            ></router-link
+          >
+
+          <router-link to="/Gerenciamento/Aluno"
+            ><v-btn variant="outlined" color="rgb(223, 70, 97)"
+              ><v-icon icon="mdi-weight-lifter" size="large"></v-icon>Alunos</v-btn
+            ></router-link
+          >
+
+          <router-link to="/Gerenciamento/Exercicios"
+            ><v-btn variant="outlined" color="rgb(223, 70, 97)"
+              ><v-icon icon="mdi-dumbbell" size="large"></v-icon>Exercícios</v-btn
+            ></router-link
+          >
+
+          <router-link to="/"
+            ><v-btn variant="outlined" color="rgb(223, 70, 97)"
+              ><v-icon icon="mdi-exit-run" size="large"></v-icon>Sair</v-btn
+            ></router-link
+          >
+        </v-toolbar-title>
+      </div>
+    </v-app-bar>
+  </v-container>
 </template>
 
 <script>
