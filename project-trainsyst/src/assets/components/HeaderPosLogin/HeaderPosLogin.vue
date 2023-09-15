184acd6b8bb2f6a2e9b54e828aa0e34e788e85a2
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-15 13:32:28 -0300

    style: update estilização do headers pós login

--- project-trainsyst/src/assets/components/HeaderPosLogin/HeaderPosLogin.vue
+++ project-trainsyst/src/assets/components/HeaderPosLogin/HeaderPosLogin.vue
@@ -2,13 +2,14 @@
   <v-app-bar height="150" app color="rgb(67, 21, 29)">
     <v-img
       src="../../../../src/public/logo trainsys.png"
-      max-weight="50"
       alt="logo TrainSys"
     />
-    <router-link to="/Dashboard"><h3>Home</h3> </router-link>
-    <router-link to="/Gerenciamento/Aluno"><h3>Alunos</h3> </router-link>
-    <router-link to="/Gerenciamento/Exercicios"><h3>Exercícios</h3> </router-link>
-    <router-link to="/"><h3>Sair</h3></router-link>
+    <v-toolbar-title>
+      <router-link to="/Dashboard">Home</router-link>
+      <router-link to="/Gerenciamento/Aluno">Alunos</router-link>
+      <router-link to="/Gerenciamento/Exercicios">Exercícios</router-link>
+      <router-link to="/">Sair</router-link>
+    </v-toolbar-title>
   </v-app-bar>
 </template>
 
