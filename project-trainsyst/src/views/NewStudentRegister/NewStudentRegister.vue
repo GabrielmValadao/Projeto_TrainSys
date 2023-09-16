3bfea6e84bc379534e2ea316060e99dc305078a3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-09-16 11:35:10 -0300

    style: finaliza a estilização da pagina de cadastro de novo aluno

--- project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
+++ project-trainsyst/src/views/NewStudentRegister/NewStudentRegister.vue
@@ -2,131 +2,136 @@
   <v-app>
     <Header></Header>
   </v-app>
-  <h2 class="d-flex justify-center align-center ma-1">
+  <h2 class="d-flex justify-center align-center ma-4">
     <v-icon
-        icon="mdi-weight-lifter"
-        color="rgb(223, 70, 97)"
-        size="large"
-      ></v-icon>Cadastro de novos alunos
+      icon="mdi-weight-lifter"
+      color="rgb(223, 70, 97)"
+      size="large"
+    ></v-icon
+    >Cadastro de novo aluno
   </h2>
 
-  <div class="d-flex justify-center align-center pa-5">
-    <v-form ref="form" @submit.prevent="handleSubmit">
-      <div class="d-flex">
-        <v-text-field
-          class="mr-5"
-          variant="underlined"
-          style="width: 300px"
-          type="text"
-          label="Nome completo"
-          v-model="name"
-          :error-messages="this.errors.name"
-        />
-        <v-text-field
-          style="width: 300px"
-          variant="underlined"
-          type="email"
-          label="Email"
-          v-model="email"
-          :error-messages="this.errors.email"
-        />
-      </div>
-      <div class="d-flex">
-        <v-text-field
-          class="mr-5"
-          variant="underlined"
-          style="width: 300px"
-          type="text"
-          label="Contato"
-          placeholder="com (DDD)"
-          v-model="contact"
-          :error-messages="this.errors.contact"
-        />
+  <div class="container">
+    <div class="d-flex justify-center align-center pa-5">
+      <v-form ref="form" @submit.prevent="handleSubmit">
+        <div class="d-flex">
+          <v-text-field
+            class="mr-5"
+            variant="underlined"
+            style="width: 300px"
+            type="text"
+            label="Nome completo"
+            v-model="name"
+            :error-messages="this.errors.name"
+          />
+          <v-text-field
+            style="width: 300px"
+            variant="underlined"
+            type="email"
+            label="Email"
+            v-model="email"
+            :error-messages="this.errors.email"
+          />
+        </div>
+        <div class="d-flex">
+          <v-text-field
+            class="mr-5"
+            variant="underlined"
+            style="width: 300px"
+            type="text"
+            label="Contato"
+            placeholder="com (DDD)"
+            v-model="contact"
+            :error-messages="this.errors.contact"
+          />
 
-        <VueDatePicker
-          style="width: 300px"
-          variant="underlined"
-          placeholder="Data de nascimento"
-          v-model="date_birth"
-          :max-date="new Date()"
-          locale="pt-BR"
-          cancelText="Cancelar"
-          selectText="Selecionar"
-          :enable-time-picker="false"
-        />
-      </div>
+          <VueDatePicker
+            style="width: 300px"
+            variant="underlined"
+            placeholder="Data de nascimento"
+            v-model="date_birth"
+            :max-date="new Date()"
+            locale="pt-BR"
+            cancelText="Cancelar"
+            selectText="Selecionar"
+            :enable-time-picker="false"
+          />
+        </div>
 
-      <h3 class="d-flex justify-center align-center ma-1">Endereço</h3>
-      <div class="d-flex">
-        <v-text-field
-          class="mr-5"
-          style="width: 300px"
-          variant="underlined"
-          type="text"
-          label="CEP"
-          v-model="cep"
-          :error-messages="this.errors.cep"
-          @blur="consultarCep"
-        />
-        <v-text-field
-          style="width: 300px"
-          type="text"
-          variant="underlined"
-          label="Logradouro"
-          v-model="street"
-          :error-messages="this.errors.street"
-        />
-      </div>
+        <h3 class="d-flex justify-center align-center ma-1">Endereço</h3>
+        <div class="d-flex">
+          <v-text-field
+            class="mr-5"
+            style="width: 300px"
+            variant="underlined"
+            type="text"
+            label="CEP"
+            v-model="cep"
+            :error-messages="this.errors.cep"
+            @blur="consultarCep"
+          />
+          <v-text-field
+            style="width: 300px"
+            type="text"
+            variant="underlined"
+            label="Logradouro"
+            v-model="street"
+            :error-messages="this.errors.street"
+          />
+        </div>
 
-      <div class="d-flex">
+        <div class="d-flex">
+          <v-text-field
+            class="mr-5"
+            style="width: 300px"
+            variant="underlined"
+            type="number"
+            label="Número"
+            v-model="number"
+            :error-messages="this.errors.number"
+          />
+          <v-text-field
+            style="width: 300px"
+            variant="underlined"
+            type="text"
+            label="Bairro"
+            v-model="neighborhood"
+            :error-messages="this.errors.neighborhood"
+          />
+        </div>
+        <div class="d-flex">
+          <v-text-field
+            class="mr-5"
+            style="width: 300px"
+            variant="underlined"
+            type="text"
+            label="Cidade"
+            v-model="city"
+            :error-messages="this.errors.city"
+          />
+          <v-text-field
+            style="width: 300px"
+            variant="underlined"
+            type="text"
+            label="Estado"
+            v-model="province"
+            :error-messages="this.errors.province"
+          />
+        </div>
         <v-text-field
-          class="mr-5"
-          style="width: 300px"
-          variant="underlined"
-          type="number"
-          label="Número"
-          v-model="number"
-          :error-messages="this.errors.number"
-        />
-        <v-text-field
-          style="width: 300px"
           variant="underlined"
           type="text"
-          label="Bairro"
-          v-model="neighborhood"
-          :error-messages="this.errors.neighborhood"
+          label="Complemento"
+          v-model="complement"
         />
-      </div>
-      <div class="d-flex">
-        <v-text-field
-          class="mr-5"
-          style="width: 300px"
-          variant="underlined"
-          type="text"
-          label="Cidade"
-          v-model="city"
-          :error-messages="this.errors.city"
-        />
-        <v-text-field
-          style="width: 300px"
-          variant="underlined"
-          type="text"
-          label="Estado"
-          v-model="province"
-          :error-messages="this.errors.province"
-        />
-      </div>
-      <v-text-field
-        variant="underlined"
-        type="text"
-        label="Complemento"
-        v-model="complement"
-      />
-      <v-btn class="mr-5" color="#DBD5B5" type="submit">Cadastrar Aluno</v-btn>
-      <router-link to="/Gerenciamento/Aluno">
-        <v-btn color="#DBD5B5">Voltar</v-btn>
-      </router-link>
-    </v-form>
+        <v-btn class="mr-5" color="#DBD5B5" type="submit"
+          >Cadastrar Aluno</v-btn
+        >
+        <router-link to="/Gerenciamento/Aluno">
+          <v-btn color="#DBD5B5">Voltar</v-btn>
+        </router-link>
+      </v-form>
+    </div>
   </div>
 </template>
 
@@ -195,7 +200,10 @@ export default {
       try {
         const schema = yup.object().shape({
           name: yup.string().required("O nome é obrigatório!"),
-          email: yup.string().email("Email não é valido").required("Email é obrigatório!"),
+          email: yup
+            .string()
+            .email("Email não é valido")
+            .required("Email é obrigatório!"),
           contact: yup.string().required("O contato é obrigatório!"),
           cep: yup.string().min(8).max(9).required("O cep é obrigatório!"),
           street: yup.string().required("O logradouro é obrigatório!"),
@@ -258,4 +266,12 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 0 auto;
+
+  width: 1000px;
+  border: 5px solid #dbd5b5;
+  border-radius: 10px;
+}
+</style>
